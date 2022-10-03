454d0a4db1e2b160f018c2f9618cd24807ecf692
Widinson Danilo <widinsondanilo@gmail.com>
2022-10-03 18:23:41 -0500

    v0.0.11

---
+++ Prto Cuentas/CuentaCorriente.js
@@ -0,0 +1,28 @@
+export class CuentaCorriente
+{
+    numero;
+    #saldo;
+    agencia;
+
+    constructor() {
+        this.#saldo = 0;
+        this.numero = '';
+        this.agencia = '';
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+}
